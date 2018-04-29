688d58159bafd8df9ecabd62f45d607b331614ba
Samuel Huang <samhuang91@gmail.com>
2018-04-29 07:50:34 -0700

    Move structs into endpoints

--- endpoints/endpoints.go
+++ endpoints/endpoints.go
@@ -11,6 +11,22 @@ import (
     "time"
 )
 
+// ApiKey is uniquely identified by content
+type ApiKey struct {
+    Id           int       `json:"id"`
+    UserId       int       `json:"user_id"`
+    Content      string    `json:"api_key"`
+    TimeCreated  time.Time `json:"time_created"`
+    TimeLastUsed time.Time `json:"time_last_used"`
+    TimeArchived time.Time `json:"time_archived"`
+}
+
+// ServerStatus has message and failures
+type ServerStatus struct {
+    Message  string   `json:"message"`
+    Failures []string `json:"failures"`
+}
+
 // GetRouter serves application
 func GetRouter() http.Handler {
     r := mux.NewRouter()
